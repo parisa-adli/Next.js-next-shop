519fc2df045b34b6ae9a0ad29281a8fe7b22b491
parisa-adli <parisa.adli1996@gmail.com>
2025-03-13 12:38:45 +0330

    font config

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import vazirFont from "@/constants/localFonts";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "next shop",
@@ -18,9 +8,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fa" dir="rtl">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${vazirFont.variable} font-sans`}
       >
         {children}
       </body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
     <h1 className="bg-primary-200 text-primary-900">
-      first load next shop app
+      اپلیکشن مدیریت پنل ادمین
     </h1>
   );
 }
